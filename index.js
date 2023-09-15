3656f6c0d43a6593648ebe69bf30575b1eed6818
Pravinkr07 <137046036+Pravinkr07@users.noreply.github.com>
2023-09-15 23:29:49 +0530

    Features
    yaah adding some features to improve attractive and responsive

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
+
+
 const ApiKey = "ef9114137a7bf9db12d87f5664180e41";
-const ApiUrl= "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}";
+const ApiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const serachindex = document.querySelector(".search input");
+const serachBtn = document.querySelector(".search button");
+const weathericon =document.querySelector(".Weather-icon");
 
-
- async function checkWeather(){
+ async function checkWeather(city){
    
-    const respone= await fetch(ApiUrl + `&appid=${ApiKey}`);
-    var data= await respone.json();
-    console.log(data);
+    const response = await fetch(ApiUrl + city +`&appid=${ApiKey}`);
+
+    if(response.status == 404 ){
+      document.querySelector(".error").style.dispaly="block";
+      document.querySelector(".Weather").style.dispaly="none";
+    }else{
+      var data = await response.json();
+
+
+
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML =Math.round( data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  
+      if(data.weather[0].main=="Clouds"){
+        weathericon.src="images/clouds.png";
+      }
+      else if(data.weather[0].main=="Clear") {
+        weathericon.src="images/clear.png";
+  
+      }
+      else if(data.weather[0].main=="Rain") {
+        weathericon.src="images/rain.png";
+  
+      }
+      else if(data.weather[0].main=="Dirizzle") {
+        weathericon.src="images/drizzle.png";
+  
+      }
+      else if(data.weather[0].main=="Mist") {
+        weathericon.src="images/mist.png";
+  
+      }
+      document.querySelector(".error").style.dispaly="none";
+
+    }
+
 
  }
- checkWeather();
+ serachBtn.addEventListener("click", ()=>{
+   checkWeather(serachindex.value);
+
+});
+
